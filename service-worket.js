228e553c8c8f94537ed061c62f92c0a642274047
mdcreator-2 <mdcreator.2.0@gmail.com>
2025-07-12 15:48:26 +0530

    Update service-worket.js

--- service-worket.js
+++ service-worket.js
@@ -1,20 +1,19 @@
+const CACHE_NAME = 'mcq-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/scripts.js'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('mcq-app').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/scripts.js'
-      ]);
-    })
-  );
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
 });
